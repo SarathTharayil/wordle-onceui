8bb3d952f67769b879878756422348832d7cb99a
Sarath Tharayil <iamsaraththarayil@gmail.com>
2025-01-11 16:59:44 +0000

    init

---
+++ src/once-ui/resources/config.js
@@ -0,0 +1,48 @@
+const baseURL = "demo.once-ui.com";
+
+// default customization applied to the HTML in the main layout.tsx
+const style = {
+  theme: "dark", // dark | light
+  neutral: "gray", // sand | gray | slate
+  brand: "cyan", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  accent: "cyan", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  solid: "contrast", // color | contrast | inverse
+  solidStyle: "flat", // flat | plastic
+  border: "playful", // rounded | playful | conservative
+  surface: "filled", // filled | translucent
+  transition: "all", // all | micro | macro
+  scaling: "100", // 90 | 95 | 100 | 105 | 110
+};
+
+// default metadata
+const meta = {
+  title: "wordle - saraththarayil.com",
+  description:
+    "",
+};
+
+// default open graph data
+const og = {
+  title: "wordle",
+  description: "",
+  type: "website",
+  image: ""
+};
+
+// default schema data
+const schema = {
+  logo: "",
+  type: "Organization",
+  name: "saraththarayil",
+  description: "",
+  email: "",
+};
+
+// social links
+const social = {
+  twitter: "https://www.twitter.com/SarathTharayil",
+  linkedin: "https://www.linkedin.com/in/SarathTharayil",
+  discord: "",
+};
+
+export { baseURL, style, meta, og, schema, social };
